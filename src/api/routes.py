7492aa9570923f3faab0e5f76b958d96270d6026
Oscar Urrutia <oscarurrutia940@gmail.com>
2022-09-30 19:04:54 +0000

    ok

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,7 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
-from flask_jwt_extended import create_access_token,JWTManager,get_jwt_identity,jwt_required,JWTManager
+from flask_jwt_extended import create_access_token,JWTManager,get_jwt, get_jwt_identity,jwt_required,JWTManager
 import redis
 
 app = Flask(__name__)
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,21 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+import { useContext } from "react";
 
 export const Navbar = () => {
+  const { store } = useContext(Context);
+
   return (
-    <nav className="navbar navbar-light bg-light">
-      <div className="container">
-        <Link to="/">
-          <span className="navbar-brand mb-0 h1">Home</span>
-        </Link>
-        <div className="ml-auto">
-          <Link to="/demo">
-            <button className="btn btn-primary">
-              Check the Context in action
-            </button>
+    store.token != null && (
+      <nav className="navbar navbar-light bg-light">
+        <div className="container">
+          <Link to="/">
+            <span className="navbar-brand mb-0 h1">Home</span>
           </Link>
+          <div className="ml-auto">
+            <Link to="/demo">
+              <button className="btn btn-primary">
+                Check the Context in action
+              </button>
+            </Link>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    )
   );
 };
